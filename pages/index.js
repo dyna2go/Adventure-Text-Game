fc66138bfd99b5975c5a053dd800f42b049e9290
dyna2go <dynamine528@gmail.com>
2022-09-11 07:08:13 -0400

    js demo test

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,21 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-import Footer from '@components/Footer'
-
+//import Footer from '@components/Footer'
+//
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Dyna2games</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <script>
+        document.getElementById("demo").innerHTML = "My First JavaScript";
+      </script>
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Test Get started by editing <code>pages/index.js</code>
+        
         </p>
       </main>
 
