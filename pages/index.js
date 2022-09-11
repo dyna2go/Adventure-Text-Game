0e8c55b86ca76d2317a983394e8412b9046b7628
dyna2go <dynamine528@gmail.com>
2022-09-11 06:54:49 -0400

    Added the word test

--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,11 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Test Get started by editing <code>pages/index.js</code>
         </p>
       </main>
 
-      <Footer />
+      
     </div>
   )
 }
