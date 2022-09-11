5065ef3d9f6b25712ce8a5b18cbb2ec43c264398
dyna2go <dynamine528@gmail.com>
2022-09-11 07:14:05 -0400

    Js test

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,18 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 //import Footer from '@components/Footer'
-//
+
 export default function Home() {
   return (
     <div className="container">
       <Head>
         <title>Dyna2games</title>
         <link rel="icon" href="/favicon.ico" />
+        
+        <script>
+        let d = new Date();
+        alert("Today's date is " + d);
+       </script>
       </Head>
 
       <main>
